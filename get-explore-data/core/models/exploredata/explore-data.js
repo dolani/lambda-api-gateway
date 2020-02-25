05f83c4ee0fc6a3834659f86289a4b4e524424d9
Ayoola Akindolani <aakindolani@eminenttechnology.com>
2020-02-25 12:12:44 +0100

    First commit

---
+++ get-explore-data/core/models/exploredata/explore-data.js
@@ -0,0 +1,17 @@
+// 'use strict';
+
+// // class exploreData {
+// //     newsHeader = ''
+// //     image = '';
+// // }
+
+// // export default exploreData;
+// module.exports = class exploreData {
+//     newsHeader = ''
+//     image = '';
+// }
+
+// // export class ExploreData {
+// //     newsHeader: String;
+// //     image: String;
+// // }
